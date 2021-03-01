d9f1111a30b48a92e09b4543f285673bfd40c865
castanc <54039892+castanc@users.noreply.github.com>
2021-03-01 18:15:59 -0300

    Create PRogram.cs

---
+++ PRogram.cs
@@ -0,0 +1,37 @@
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30517.126
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "PDFExtractor", "PDFExtractor.csproj", "{2F5DA2B2-6767-415C-B70A-4980FCC3C487}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "PDFReader", "..\PDFReader\PDFReader.csproj", "{AEA2031C-2407-430C-A018-59509104DB46}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "PDFTextReader", "..\PDFTextReader\PDFTextReader.csproj", "{970386DB-8B05-43C5-9E0B-91C33A70E928}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{2F5DA2B2-6767-415C-B70A-4980FCC3C487}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{2F5DA2B2-6767-415C-B70A-4980FCC3C487}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{2F5DA2B2-6767-415C-B70A-4980FCC3C487}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{2F5DA2B2-6767-415C-B70A-4980FCC3C487}.Release|Any CPU.Build.0 = Release|Any CPU
+		{AEA2031C-2407-430C-A018-59509104DB46}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{AEA2031C-2407-430C-A018-59509104DB46}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{AEA2031C-2407-430C-A018-59509104DB46}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{AEA2031C-2407-430C-A018-59509104DB46}.Release|Any CPU.Build.0 = Release|Any CPU
+		{970386DB-8B05-43C5-9E0B-91C33A70E928}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{970386DB-8B05-43C5-9E0B-91C33A70E928}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{970386DB-8B05-43C5-9E0B-91C33A70E928}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{970386DB-8B05-43C5-9E0B-91C33A70E928}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {F3721E0F-1296-446A-B75E-82FE1FEC4918}
+	EndGlobalSection
+EndGlobal
